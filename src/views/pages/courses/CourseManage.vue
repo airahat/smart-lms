d173f5fd1ce8243d1421981a0881f186ce40bce6
tanvirrahman005 <mdtanvirrahman005@gmail.com>
2025-12-20 13:16:43 +0600

    changed courses layout

--- src/views/pages/courses/CourseManage.vue
+++ src/views/pages/courses/CourseManage.vue
@@ -8,10 +8,10 @@ import { useUserStore } from '@/store/user';
 const userStore = useUserStore();
 
 onMounted(() => {
-  if (!userStore.user) {
-    const localUser = JSON.parse(localStorage.getItem('user') || 'null');
-    if (localUser) userStore.setUser(localUser);
-  }
+    if (!userStore.user) {
+        const localUser = JSON.parse(localStorage.getItem('user') || 'null');
+        if (localUser) userStore.setUser(localUser);
+    }
 });
 
 const courses = ref<Course[]>([defaultCourse]);
@@ -31,15 +31,15 @@ fetchCourses();
 
 
 function deleteCourse(id: number) {
-  if (!confirm('Are you sure you want to delete this course?')) return;
+    if (!confirm('Are you sure you want to delete this course?')) return;
 
-  api.delete(`courses/${id}`)
-    .then(() => {
-      courses.value = courses.value.filter(c => c.id !== id);
-    })
-    .catch(err => {
-      console.error('Delete failed:', err);
-    });
+    api.delete(`courses/${id}`)
+        .then(() => {
+            courses.value = courses.value.filter(c => c.id !== id);
+        })
+        .catch(err => {
+            console.error('Delete failed:', err);
+        });
 }
 
 </script>
@@ -47,48 +47,75 @@ function deleteCourse(id: number) {
 <template>
     <div class="container-fluid">
 
-        <div>
-            <div class="bg-success text-light fs-4 fw-bolder d-flex p-2"><span>Courses List</span> <router-link
-                    to="/courses/create" class="btn btn-outline-info text-light fw-bolder ms-auto">Add
-                    New+</router-link>
+
+        <!-- <div class="bg-success text-light fs-4 fw-bolder d-flex p-2"><span>Courses List</span> <router-link
+                to="/courses/create" class="btn btn-outline-info text-light fw-bolder ms-auto">Add
+                New+</router-link>
+        </div> -->
+        <!-- <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>SL</th>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Instructor</th>
+                    <th>Duration (Hrs.)</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(course, index) in courses" :key="course.id">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ course.course_code }}</td>
+                    <td>{{ course.title }}</td>
+                    <td>{{ course.trainer_name }}</td>
+                    <td>{{ course.duration }}</td>
+                    <td>
+                        <router-link :to="`/courses/${course.id}/details`" class="btn btn-sm"
+                            v-if="userStore.isAdmin"><i class="fa-solid fa-eye fs-5 "></i></router-link>
+                        <router-link :to="`/courses/${course.id}/edit`" class="btn btn-sm" v-if="userStore.isAdmin"><i
+                                class="fa-solid fs-5 fa-pen-to-square text-info"></i></router-link>
+                        <button @click="deleteCourse(course.id)" class="btn btn-sm" v-if="userStore.isAdmin"><i
+                                class="fa-regular fa-trash-can fs-5 text-danger"></i></button>
+                        <router-link :to="`/courses/${course.id}/details`" class="btn btn-sm" v-else><i
+                                class="fa-solid fa-eye fs-5 "></i></router-link>
+                    </td>
+                </tr>
+
+            </tbody>
+
+
+        </table> -->
+        <div class="d-flex flex-wrap ">
+
+            <div class="card col-2 mb-3 ms-3" v-for="(course, index) in courses" :key="course.id">
+                <div class="card-header bg-success text-white w-100">
+                    <h5 class="card-title">{{ course.title }}</h5>
+
+                </div>
+                <div class="card-body">
+
+
+                    <p class="card-text"><strong>Course Code:</strong> {{ course.course_code }}</p>
+                    <p class="card-text"><strong>Instructor:</strong> {{ course.trainer_name }}</p>
+                    <p class="card-text"><strong>Duration:</strong> {{ course.duration }} hours</p>
+                    <div class="card-footer d-flex justify-content-between">
+
+                        <router-link :to="`/courses/${course.id}/edit`" class="btn btn-primary me-2"
+                            v-if="userStore.isAdmin">Edit Course</router-link>
+                        <button @click="deleteCourse(course.id)" class="btn btn-danger" v-if="userStore.isAdmin">Delete
+                            Course</button>
+                    </div>
+
+
+                </div>
             </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>SL</th>
-                        <th>Course Code</th>
-                        <th>Course Name</th>
-                        <th>Instructor</th>
-                        <th>Duration (Hrs.)</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(course, index) in courses" :key="course.id">
-                        <td>{{ index + 1 }}</td>
-                        <td>{{ course.course_code }}</td>
-                        <td>{{ course.title }}</td>
-                        <td>{{ course.trainer_name }}</td>
-                        <td>{{ course.duration }}</td>
-                        <td>
-                            <router-link :to="`/courses/${course.id}/details`" class="btn btn-sm" v-if="userStore.isAdmin"><i
-                                    class="fa-solid fa-eye fs-5 "></i></router-link>
-                                    <router-link :to="`/courses/${course.id}/edit`" class="btn btn-sm" v-if="userStore.isAdmin"><i
-                                        class="fa-solid fs-5 fa-pen-to-square text-info"></i></router-link>
-                                        <button @click="deleteCourse(course.id)" class="btn btn-sm" v-if="userStore.isAdmin"><i
-                                            class="fa-regular fa-trash-can fs-5 text-danger"></i></button>
-                                            <router-link :to="`/courses/${course.id}/details`" class="btn btn-sm" v-else><i
-                                                    class="fa-solid fa-eye fs-5 "></i></router-link>
-                        </td>
-                    </tr>
-
-                </tbody>
 
 
-            </table>
         </div>
 
     </div>
+
 </template>
 
 <style scoped></style>
